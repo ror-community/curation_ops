6f334358666bd8663d8e87096e940416588083f2
Adam Buttrick <adambuttrick@Adams-MacBook-Pro-2.local>
2022-06-08 17:30:48 -0700

    fixing some bugs in create_relationships_from_github/create_realtionships.py and addressing new approach to relationships with iterative release commits

--- create_relationships_from_github/create_relationships.py
+++ create_relationships_from_github/create_relationships.py
@@ -13,14 +13,26 @@ GITHUB['TOKEN'] = ''
 # Input here is a CSV file containing the Github issue url, ROR ID, 
 # and name for each file in the release, both new and update records.
 def dict_from_csv(f):
-    url_names_ids = {}
+    ids_k_names_v, names_k_ids_v = {}, {}
     release_ids = []
     with open(f, encoding='utf-8-sig') as f_in:
-        reader = csv.reader(f_in)
+        reader = csv.DictReader(f_in)
         for row in reader:
-            release_ids.append(row[1])
-            url_names_ids[row[0]] = [row[1], row[2]]
-    return release_ids, url_names_ids
+            release_ids.append(row['ror_id'])
+            ids_k_names_v[row['ror_id']] = row['name']
+            names_k_ids_v[row['name']] = row['ror_id']
+    return release_ids, ids_k_names_v, names_k_ids_v
+
+
+def find_between(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        match = s[start:end]
+        match = match.strip()
+        return match
+    except ValueError:
+        return ''
 
 
 def get_issue_comments(comments_url):
@@ -43,9 +55,9 @@ def get_ror_name(ror_id):
 
 
 def extract_relationships(f):
-    pages = [1, 2, 3, 4, 5, 6, 7, 8]
+    pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     api_urls = []
-    release_ids, release_urls_names_ids = dict_from_csv(f)
+    release_ids, ids_k_names_v, names_k_ids_v = dict_from_csv(f)
     outfile = os.getcwd() + '/relationships.csv'
     header = ['Issue # from Github', 'Issue URL', 'Issue title from Github', 'Name of org in Record ID', 'Record ID',
               'Related ID', 'Name of org in Related ID', 'Relationship of Related ID to Record ID', 'Current location of Related ID']
@@ -53,7 +65,7 @@ def extract_relationships(f):
         writer = csv.writer(f_out)
         writer.writerow(header)
     # approved column url
-    url = 'https://api.github.com/projects/columns/13954326/cards'
+    url = 'https://api.github.com/projects/columns/18464412/cards'
     for page in pages:
         params = {'page': page, 'per_page': 100}
         cards = requests.get(url, auth=(
@@ -67,24 +79,32 @@ def extract_relationships(f):
         issue_number = issue_data['number']
         issue_title = issue_data['title']
         org_name, org_ror_id = '', ''
-        issue_text = issue_data['body'] + \
+        issue_body = issue_data['body']
+        issue_text = issue_body + \
             get_issue_comments(api_url + '/comments')
         issue_html_url = issue_data['html_url']
         rel_pattern = re.compile(
             r'[https]{0,5}\:\/\/ror\.org\/[a-z0-9]{9}\s+\([a-zA-Z]{0,}\)')
         relationships = rel_pattern.findall(issue_text)
         if relationships != []:
-            org_name = release_urls_names_ids[issue_html_url][1]
-            org_ror_id = release_urls_names_ids[issue_html_url][0]
-            search_ror_id = re.search(r'(?<=ROR ID\:)(.*)(?=\n)', issue_text)
+            org_ror_id = find_between(issue_body, 'ROR ID:', '\n')
+            org_name = find_between(issue_body, 'Name of organization:','\n')
+            if org_ror_id == '':
+                org_ror_id = names_k_ids_v[org_name]
             for relationship in relationships:
                 relationship = relationship.split(' ')
-                relationships = [r.strip() for r in relationships if r != '']
+                relationship = [r.strip() for r in relationship if r != '']
                 related_ror_id = relationship[0].strip()
                 relationship_type = relationship[1].strip().lower()
                 relationship_type = re.sub(r'[()]', '', relationship_type)
                 relationship_type = relationship_type.capitalize()
-                related_name = get_ror_name(related_ror_id)
+                if related_ror_id in release_ids:
+                    try:
+                        related_name = ids_k_names_v[related_ror_id]
+                    except KeyError:
+                        related_name == ''
+                else:
+                    related_name = get_ror_name(related_ror_id)
                 with open(outfile, 'a') as f_out:
                     locations = ['Release', 'Release'] if related_ror_id in release_ids else ['Production', 'Release']
                     rel_type_mappings = {'Parent': 'Child', 'Child': 'Parent',
