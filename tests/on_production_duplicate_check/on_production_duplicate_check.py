b4e3ca8c5c9280a56795603c02a2ae1d4e35e689
Adam Buttrick <adambuttrick@Adams-MacBook-Pro-2.local>
2022-07-28 20:02:27 -0700

    Adding fixes for on_production_duplicate_check.py

--- tests/on_production_duplicate_check/on_production_duplicate_check.py
+++ tests/on_production_duplicate_check/on_production_duplicate_check.py
@@ -1,21 +1,27 @@
 import os
+import re
 import sys
+import csv
 import json
 import glob
 import urllib
 import itertools
 import requests
+import string
 from thefuzz import fuzz
 
 def clean_org_name(org_name):
     org_name = org_name.lower()
+    org_name = re.sub(r'[^\w\s]', '', org_name)
+    exclude = set(string.punctuation)
+    org_name = ''.join(ch for ch in org_name if ch not in exclude)
     return org_name
 
 def ror_search(org_name):
     query_url = 'https://api.ror.org/organizations?query="' + \
-       org_name + '"'
+       urllib.parse.quote_plus(org_name) + '"'
     affiliation_url =  'https://api.ror.org/organizations?affiliation="' + \
-       org_name + '"'
+       urllib.parse.quote_plus(org_name) + '"'
     all_urls = [query_url, affiliation_url]
     ror_matches = []
     for url in all_urls:
@@ -34,18 +40,18 @@ def ror_search(org_name):
                 name_mr = fuzz.ratio(clean_org_name(org_name), clean_org_name(ror_name))
                 if name_mr >= 90:
                     match_type = 'name match'
-                    ror_matches.append([ror_id, ror_name, match_type])
-                for alias in record['aliases']:
+                    ror_matches.append([ror_id, ror_name, match_type, name_mr])
+                for alias in result['aliases']:
                     alias_mr = fuzz.ratio(clean_org_name(org_name), clean_org_name(alias))
                     if alias_mr >= 90:
                         match_type = 'alias match'
-                        ror_matches.append([ror_id, ror_name, match_type])
-                for label in record['labels']:
+                        ror_matches.append([ror_id, ror_name, match_type, alias_mr])
+                for label in labels:
                     label_mr = fuzz.ratio(clean_org_name(org_name), clean_org_name(label))
                     if label_mr >= 90:
                         match_type = 'label match'
-                        ror_matches.append([ror_id, ror_name, match_type])
-                elif 'relationships' in result:
+                        ror_matches.append([ror_id, ror_name, match_type, label_mr])
+                if 'relationships' in result:
                     for relationship in result['relationships']:
                         rel_mr = fuzz.ratio(clean_org_name(org_name), clean_org_name(relationship['label']))
                         if rel_mr >= 90:
@@ -55,6 +61,11 @@ def ror_search(org_name):
     return ror_matches
 
 def search_json():
+    header = ["ror_id", "name", "matched_ror_id", "matched_name", "match_type", "match_ratio"]
+    outfile = "ror_matches.csv"
+    with open(outfile, 'w') as f_out:
+        writer = csv.writer(f_out)
+        writer.writerow(header)
     for file in glob.glob("*.json"):
         with open(file, 'r+') as f_in:
             json_file = json.load(f_in)
@@ -66,7 +77,14 @@ def search_json():
         if json_file['labels'] != []:
             all_names += [label['label'] for label in json_file['labels']]
         for name in all_names:
-            ror_search(name)
+            print("Searching", ror_id, "-", name, "...")
+            ror_matches = ror_search(name)
+            if ror_matches != []:
+                for match in ror_matches:
+                    with open(outfile, 'a') as f_out:
+                        writer = csv.writer(f_out)
+                        writer.writerow([ror_id, name] + match)
+
 
 if __name__ == '__main__':
     search_json()
