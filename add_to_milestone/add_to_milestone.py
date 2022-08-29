e4d1995c728e5d99c94db3c9497f9397c1eb968c
Adam Buttrick <adambuttrick@Adams-MacBook-Pro-2.local>
2022-08-29 10:44:49 -0700

    Switching add_to_milestone.py to reference Ready for Production Release column instead of CSV

--- add_to_milestone/add_to_milestone.py
+++ add_to_milestone/add_to_milestone.py
@@ -6,17 +6,21 @@ from github import Github
 
 GITHUB = {}
 GITHUB['TOKEN'] = ''
+MILESTONE = 
 
-def add_to_milestone(f, milestone):
+def add_to_milestone():
     g = Github(GITHUB['TOKEN'])
-    with open(f, encoding='utf-8-sig') as f_in:
-        reader = csv.DictReader(f_in)
-        for row in reader:
-            repo = g.get_repo("ror-community/ror-updates")
-            issue_number = int(row['issue_number'])
-            issue = repo.get_issue(number=issue_number)
-            issue.edit(milestone=repo.get_milestone(int(milestone))
+    repo = g.get_repo("ror-community/ror-updates")
+    project = repo.get_projects()[0]
+    columns = project.get_columns()
+    prod_release_column = [column for column in columns if column.name ==
+               'Ready for production release'][0]
+    prod_release_cards = prod_release_column.get_cards()
+    for card in prod_release_cards:
+        issue = card.get_content()
+        if issue is not None:
+            issue.edit(milestone=repo.get_milestone(MILESTONE))
 
 
 if __name__ == '__main__':
-    update_issues_for_release(sys.argv[1], sys.argv[2])
+    add_to_milestone()
