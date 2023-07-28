56fc2d50e31a67b7d379586ae2d9db9a98095038
lizkrznarich <liz.krznarich@datacite.org>
2023-07-28 16:52:44 -0500

    first pass at v1 to v2 crosswalk

---
+++ v2_crosswalk/convert_v1_to_v2.py
@@ -0,0 +1,250 @@
+import argparse
+import copy
+import json
+import os
+import logging
+import sys
+import re
+from datetime import date
+
+import v2_fields
+import v2_enums
+
+TODAY = date.today()
+ERROR_LOG = "errors.log"
+INPUT_PATH = "./V1_INPUT/"
+OUTPUT_PATH = "./V2_OUTPUT/"
+V2_TEMPLATE = "./v2_template.json"
+
+logging.basicConfig(filename=ERROR_LOG,level=logging.ERROR, filemode='w')
+
+
+# admin
+def format_admin():
+    v2_admin = v2_fields.v2_admin_tempalte
+    v2_admin['created']['date'] = str(TODAY)
+    v2_admin['created']['schema_version'] = copy.copy(v2_enums.SCHEMA_VERSIONS['1'])
+    v2_admin['last_modified']['date'] = str(TODAY)
+    v2_admin['last_modified']['schema_version'] = copy.copy(v2_enums.SCHEMA_VERSIONS['2'])
+    return v2_admin
+
+# relationships
+def format_relationships(v1_relationships):
+    for v1_rel in v1_relationships:
+        v1_rel['type'] = v1_rel['type'].lower()
+    return v1_relationships
+
+# external_ids
+def get_v2_ext_id_type(v1_ext_id_type):
+    type_key = "".join([k for k in v2_enums.EXTERNAL_ID_TYPES if v2_enums.EXTERNAL_ID_TYPES[k]==v1_ext_id_type.lower()])
+    if type_key:
+        return v2_enums.EXTERNAL_ID_TYPES[type_key]
+    else:
+        return None
+
+def format_external_ids(v1_external_ids):
+    v2_external_ids = []
+    for k in v1_external_ids:
+        print("v1 key is:")
+        print(k)
+        v2_ext_id_type = get_v2_ext_id_type(k)
+        if v2_ext_id_type:
+            v2_external_id = copy.deepcopy(v2_fields.v2_external_id_template)
+            print("starting v2 ext id is:")
+            print(v2_external_id)
+            v2_external_id['type'] = v2_ext_id_type
+            if isinstance(v1_external_ids[k]['all'], list) and len(v1_external_ids[k]['all']) > 0:
+                v2_external_id['all'] = v1_external_ids[k]['all']
+            if isinstance(v1_external_ids[k]['all'], str) and len(v1_external_ids[k]['all']) > 0:
+                v2_external_id['all'].append(v1_external_ids[k]['all'])
+            if v1_external_ids[k]['preferred']:
+                v2_external_id['preferred'] = v1_external_ids[k]['preferred']
+            if v2_external_id['preferred'] or v2_external_id['all']:
+                v2_external_ids.append(v2_external_id)
+    return v2_external_ids
+
+# links
+def format_links(v1_links, v1_wikipedia_url):
+    v2_links = []
+    v1_links = filter(None, v1_links)
+    for link in v1_links:
+        v2_link = copy.deepcopy(v2_fields.v2_link_template)
+        v2_link['value'] = link
+        v2_link['type'] = copy.copy(v2_enums.LINK_TYPES['WEBSITE'])
+        v2_links.append(v2_link)
+    if v1_wikipedia_url:
+        v2_link = copy.deepcopy(v2_fields.v2_link_template)
+        v2_link['value'] = v1_wikipedia_url
+        v2_link['type'] = copy.copy(v2_enums.LINK_TYPES['WIKIPEDIA'])
+    return v2_links
+
+# locations
+def format_locations(v1_data):
+    v2_locations = []
+    for address in v1_data['addresses']:
+        v2_location = copy.deepcopy(v2_fields.v2_location_template)
+        v2_location['geonames_id'] = address['geonames_city']['id']
+        if v1_data['country']['country_code']:
+            v2_location['geonames_details']['country_code'] = v1_data['country']['country_code']
+        if v1_data['country']['country_name']:
+            v2_location['geonames_details']['country_name'] = v1_data['country']['country_name']
+        if address['lat']:
+            v2_location['geonames_details']['lat'] = address['lat']
+        if address['lng']:
+            v2_location['geonames_details']['lng'] = address['lng']
+        v2_location['geonames_details']['name'] = address['geonames_city']['city']
+        v2_locations.append(v2_location)
+    return v2_locations
+
+# names
+def format_names(v1_data):
+    v2_names = []
+
+    if v1_data['name']:
+        v2_name = copy.deepcopy(v2_fields.v2_name_template)
+        v2_name['value'] = v1_data['name']
+        v2_name['types'].extend([v2_enums.NAME_TYPES['ROR_DISPLAY'], v2_enums.NAME_TYPES['LABEL']])
+        v2_names.append(v2_name)
+
+    if len(v1_data['aliases']) > 0:
+        for alias in v1_data['aliases']:
+            v2_alias = copy.deepcopy(v2_fields.v2_name_template)
+            v2_alias['value'] = alias
+            v2_alias['types'].append(v2_enums.NAME_TYPES['ALIAS'])
+            v2_names.append(v2_alias)
+
+    if len(v1_data['labels']) > 0:
+        for label in v1_data['labels']:
+            v2_label = copy.deepcopy(v2_fields.v2_name_template)
+            v2_label['value'] = label['label']
+            v2_label['lang'] = label['iso639']
+            v2_label['types'].append(v2_enums.NAME_TYPES['LABEL'])
+            v2_names.append(v2_label)
+
+    if len(v1_data['acronyms']) > 0:
+        for acronym in v1_data['acronyms']:
+            v2_acronym = copy.deepcopy(v2_fields.v2_name_template)
+            v2_acronym['value'] = acronym
+            v2_acronym['types'].append(v2_enums.NAME_TYPES['ACRONYM'])
+            v2_names.append(v2_acronym)
+
+    return v2_names
+
+def convert_v1_to_v2(v1_data):
+    #try:
+    with open(V2_TEMPLATE) as template_file:
+        v2_data = json.load(template_file)
+        # these fields don't change
+        v2_data['id'] = v1_data['id']
+        v2_data['types'] = [type.lower() for type in v1_data['types']]
+        v2_data['status'] = v1_data['status']
+        v2_data['established'] = v1_data['established']
+        v2_data['domains'] = []
+        # these fields DO change
+        v2_data['relationships'] = format_relationships(v1_data['relationships'])
+        v2_data['external_ids'] = format_external_ids(v1_data['external_ids'])
+        v2_data['links'] = format_links(v1_data['links'], v1_data['wikipedia_url'])
+        v2_data['locations'] = format_locations(v1_data)
+        v2_data['names'] = format_names(v1_data)
+        v2_data['admin'] = format_admin()
+        return v2_data
+    #except Exception as e:
+    #    logging.error(f"Error converting v1 data to v2: {e}")
+
+
+def create_v2_file(v1_file):
+    #try:
+    with open(v1_file) as infile:
+        v1_data = json.load(infile)
+        ror_id = re.sub('https://ror.org/', '', v1_data['id'])
+        v2_record_data = convert_v1_to_v2(v1_data)
+    with open(OUTPUT_PATH + ror_id + ".json", "w") as writer:
+        writer.write(
+        json.dumps(v2_record_data, indent=4, separators=(',', ': '))
+        )
+    #except Exception as e:
+    #    logging.error(f"Error concatenating files: {e}")
+
+def validate(input, version, check_address, rel_file = None, path = None, schema = None):
+    """Runs the files against the schema validator and the class that checks the usecases"""
+    files = get_files(input)
+    filename = ""
+    validation_errors = False
+    errors = []
+
+    if (rel_file and not(path)):
+        raise AttributeError(f"Relationship file: {rel_file} must be passed with a --file-path argument which is a path to the rest of the files for relationship validation")
+    elif (rel_file and path):
+        u.arg_exists(rel_file)
+
+    for f in files:
+        messages = {}
+        filename = os.path.basename(f).split(".")[0]
+        valid = True
+        valid, msg = vs.validate_file(f,schema,version)
+        if valid:
+            print("schema valid")
+            messages[filename] = run_validation_tests(f, version, check_address, path, rel_file)
+            if len(messages[filename]) == 0:
+                messages[filename] = None
+        else:
+            print("NOT schema valid")
+            messages[filename] = msg
+
+        if messages[filename]:
+            errors.append(deepcopy(messages))
+
+    if len(errors) > 0:
+        validation_errors = print_errors(errors, validation_errors)
+
+    if validation_errors:
+        with open(ERROR_LOG, 'r') as f:
+            print(f.read())
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+def get_files(input):
+    files = []
+    if os.path.isfile(input):
+        files.append(input)
+    elif os.path.isdir(input):
+        file = []
+        path = os.path.normpath(input)
+        for f in os.listdir(input):
+            file.append(f)
+        files = list(map(lambda x: path+"/"+x, file))
+    else:
+        raise RuntimeError(f"{input} must be a valid file or directory")
+    return files
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Script to generate v2 ROR record from v1 record")
+    parser.add_argument('-i', '--inputpath', type=str, default='./V1_INPUT')
+    parser.add_argument('-o', '--outputpath', type=str, default='./V2_OUTPUT')
+    args = parser.parse_args()
+    global INPUT_PATH
+    global OUTPUT_PATH
+
+    files = get_files(args.inputpath)
+    print(files)
+
+    if files:
+        for file in files:
+            print("processing " + file)
+            create_v2_file(file)
+    else:
+        print("No files exist in " + INPUT_PATH)
+
+    file_size = os.path.getsize(ERROR_LOG)
+    if (file_size == 0):
+        os.remove(ERROR_LOG)
+    elif (file_size != 0):
+        with open(ERROR_LOG, 'r') as f:
+            print(f.read())
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
---
+++ v2_crosswalk/v2_enums.py
@@ -0,0 +1,49 @@
+SCHEMA_VERSIONS = {
+                        "1" : "1.0",
+                        "2" : "2.0"
+                    }
+
+EXTERNAL_ID_TYPES = {
+                        "FUNDREF" : "fundref",
+                        "GRID" : "grid",
+                        "ISNI" : "isni",
+                        "WIKIDATA" : "wikidata"
+                    }
+
+LINK_TYPES = {
+                "WEBSITE" : "website",
+                "WIKIPEDIA" : "wikipedia"
+            }
+
+NAME_TYPES = {
+                "ACRONYM" : "acronym",
+                "ALIAS" : "alias",
+                "LABEL" : "label",
+                "ROR_DISPLAY" : "ror_display"
+            }
+
+ORG_TYPES = {
+                "EDUCATION" : "education",
+                "FUNDER" : "funder",
+                "HEALTHCARE" : "healthcare",
+                "COMPANY" : "company",
+                "ARCHIVE" : "archive",
+                "NONPROFIT" : "nonprofit",
+                "GOVERNMENT" : "government",
+                "FACILITY" : "facility",
+                "OTHER" : "other"
+            }
+
+RELATIONSHIP_TYPES = {
+                "RELATED" : "related",
+                "PARENT" : "parent",
+                "CHILD" : "child",
+                "SUCCESSOR" : "successor",
+                "PREDECESSOR" : "predecessor"
+            }
+
+STATUSES = {
+                "ACTIVE" : "active",
+                "INACTIVE" : "inactive",
+                "WITHDRAWN" : "withdrawn"
+            }
