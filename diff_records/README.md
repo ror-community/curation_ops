fa2497fc8e076b79323a8bbbc5f9a14ebecfac38
adambuttrick <adambuttrick@gmail.com>
2024-03-06 14:52:24 -0800

    Adding v2 version of diff records

--- diff_records/README.md
+++
@@ -1,9 +0,0 @@
-# diff_updates
-Quick script for diffing ROR record updates.
-
-# usage
-Copy to the directory with the ROR records JSON:
-
-pip install -r requirements.txt
-
-python diff.py
--- diff_records/diff.py
+++ diff_records/diff.py
@@ -3,35 +3,50 @@ import csv
 import sys
 import json
 import glob
+import argparse
 import requests
 import jsondiff
 
-def get_diffs():
-	curr_dir =  os.getcwd()
-	outfile = curr_dir + '/diff_report.csv'
-	header = ['ror_id', 'field', 'change']
-	with open(outfile, 'w') as f_out:
-		writer = csv.writer(f_out)
-		writer.writerow(header)
-	for file in glob.glob("*.json"):
-		with open(file, 'r+') as f_in:
-			json_file = json.load(f_in)
-		ror_id = json_file['id']
-		api_url  = 'http://localhost:9292/organizations/' + ror_id
-		api_json = requests.get(api_url).json()
-		file_api_diffs = jsondiff.diff(api_json, json_file, syntax='symmetric')
-		if type(file_api_diffs) == list:
-			for diff in file_api_diffs:
-				for key, value in diff.items():
-					with open(outfile, 'a') as f_out:
-						writer = csv.writer(f_out)
-						writer.writerow([ror_id, key, value])
-		else:
-			for key, value in file_api_diffs.items():
-				with open(outfile, 'a') as f_out:
-					writer = csv.writer(f_out)
-					writer.writerow([ror_id, key, value])
+
+def get_diffs(input_dir, output_file):
+    header = ['ror_id', 'field', 'change']
+    with open(output_file, 'w') as f_out:
+        writer = csv.writer(f_out)
+        writer.writerow(header)
+    for file in glob.glob(f"{input_dir}/*.json"):
+        with open(file, 'r+') as f_in:
+            json_file = json.load(f_in)
+        ror_id = json_file['id']
+        api_url = f'https://api.ror.org/v2/organizations/{ror_id}'
+        api_json = requests.get(api_url).json()
+        file_api_diffs = jsondiff.diff(api_json, json_file, syntax='symmetric')
+        if type(file_api_diffs) == list:
+            for diff in file_api_diffs:
+                for key, value in diff.items():
+                    with open(output_file, 'a') as f_out:
+                        writer = csv.writer(f_out)
+                        writer.writerow([ror_id, key, value])
+        else:
+            for key, value in file_api_diffs.items():
+                with open(output_file, 'a') as f_out:
+                    writer = csv.writer(f_out)
+                    writer.writerow([ror_id, key, value])
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Compare records on production to files in a directory containing ROR records")
+    parser.add_argument("-i", "--input_dir", required=True,
+                        help="Input directory path.")
+    parser.add_argument("-o", "--output_file",
+                        default="diffs.csv", help="Output CSV file path.")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    get_diffs(args.input_dir, args.output_file)
+
 
 if __name__ == '__main__':
-	get_diffs()
-
+    main()
