2a7647ef46fa1830523298b2e435a09025918662
adambuttrick <adambuttrick@gmail.com>
2024-04-12 23:16:30 -0700

    Update add_languages_to_data_dump.py
    Add copy of updated files to separate directory

--- add_languages_to_data_dump/add_languages_to_data_dump/add_languages_to_data_dump.py
+++ add_languages_to_data_dump/add_languages_to_data_dump/add_languages_to_data_dump.py
@@ -1,3 +1,5 @@
+import os
+import re
 import csv
 import json
 import argparse
@@ -23,7 +25,16 @@ def parse_json(file_path):
     return data_dump
 
 
-def update_json_records(data_dump, csv_data):
+def save_individual_json(directory, record):
+    record_id = record['id']
+    file_prefix = re.sub('https://ror.org/', '', record_id)
+    file_name = f'{file_prefix}.json'
+    file_path = os.path.join(directory, file_name)
+    with open(file_path, 'w') as file:
+        json.dump(record, file, indent=4, ensure_ascii=False)
+
+
+def update_json_records(data_dump, csv_data, updates_dir):
     for record in data_dump:
         record_id = record['id']
         if record_id in csv_data:
@@ -31,12 +42,13 @@ def update_json_records(data_dump, csv_data):
                 for name, lang in csv_data[record_id]:
                     if name_obj['value'] == name:
                         name_obj['lang'] = lang
+            save_individual_json(updates_dir, record)
     return data_dump
 
 
 def save_json(file_path, data_dump):
     with open(file_path, 'w') as file:
-        json.dump(data_dump, file, indent=4)
+        json.dump(data_dump, file, indent=4, ensure_ascii=False)
 
 
 def parse_args():
@@ -46,8 +58,10 @@ def parse_args():
                         help='Path to the input CSV file')
     parser.add_argument('-d', '--data_dump_file',
                         required=True, help='Path to the input JSON file')
-    parser.add_argument('-o', '--output_file', default='dump_w_languages_added.json',
+    parser.add_argument('-o', '--output_file', required=True,
                         help='Path to the output JSON file')
+    parser.add_argument('-u', '--updates_dir', default='updates',
+                        help='Directory to save individual JSON files')
     return parser.parse_args()
 
 
@@ -55,9 +69,12 @@ def main():
     args = parse_args()
     csv_data = parse_csv(args.input_file)
     data_dump = parse_json(args.data_dump_file)
-    updated_data_dump = update_json_records(data_dump, csv_data)
+    os.makedirs(args.updates_dir, exist_ok=True)
+    updated_data_dump = update_json_records(
+        data_dump, csv_data, args.updates_dir)
     save_json(args.output_file, updated_data_dump)
     print(f'Updated JSON records saved to {args.output_file}')
+    print(f'Individual JSON files saved in {args.updates_dir} directory')
 
 
 if __name__ == '__main__':
