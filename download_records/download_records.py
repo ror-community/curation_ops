42e56c8984b1f61bbaa26b1dd30fe137dc5f4226
adambuttrick <adambuttrick@gmail.com>
2024-04-14 16:25:53 -0700

    Update download records to v2

--- download_records/download_records.py
+++ download_records/download_records.py
@@ -4,30 +4,41 @@ import sys
 import csv
 import json
 import requests
+import argparse
 from datetime import datetime
 
 
+def download_record(ror_id, schema_version, json_file_path):
+    api_url = f'https://api.ror.org/v{schema_version}/organizations/{ror_id}'
+    ror_data = requests.get(api_url).json()
+    with open(json_file_path, 'w', encoding='utf8') as f_out:
+        json.dump(ror_data, f_out, indent=4, ensure_ascii=False)
 
-def download_record(ror_id, json_file_path):
-	api_url = 'http://localhost:9292/organizations/' + ror_id
-	ror_data = requests.get(api_url).json()
-	with open(json_file_path, 'w', encoding='utf8') as f_out:
-		json.dump(ror_data, f_out, indent=4, ensure_ascii=False)
 
 def parse_and_download(f):
-	now = datetime.now()
-	json_dir = os.getcwd() + '/' + now.strftime("%Y%m%d_%H%M%S") + '/'
-	os.makedirs(json_dir)
-	with open(f, encoding='utf-8-sig') as f_in:
-		reader = csv.DictReader(f_in)
-		for row in reader:
-			ror_id = row['ror_id']
-			print('Downloading', ror_id, '...')
-			ror_id = ror_id.split('.org/')[1]
-			json_file_path = ror_id + '.json'
-			json_file_path = json_dir + json_file_path
-			download_record(ror_id, json_file_path)
+    now = datetime.now()
+    json_dir = os.getcwd() + '/' + now.strftime("%Y%m%d_%H%M%S") + '/'
+    os.makedirs(json_dir)
+    with open(f, encoding='utf-8-sig') as f_in:
+        reader = csv.DictReader(f_in)
+        for row in reader:
+            ror_id = row['id']
+            print('Downloading', ror_id, '...')
+            ror_id = ror_id.split('.org/')[1]
+            json_file_path = ror_id + '.json'
+            json_file_path = json_dir + json_file_path
+            download_record(ror_id, json_file_path)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Download ROR records')
+    parser.add_argument('-i', '--input_file', type=str,
+                        help='Path to the input CSV file')
+    parser.add_argument('-s', '--schema_version', type=str,
+                        choice=["1", "2"], help='Schema version for the records to download. Choices: 1 or 2')
+    args = parser.parse_args()
+    parse_and_download(args.input_file, schema_version)
+
 
 if __name__ == '__main__':
-	parse_and_download(sys.argv[1])
-
+    main()
