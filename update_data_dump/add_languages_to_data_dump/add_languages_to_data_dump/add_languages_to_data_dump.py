667e5fe2cf4405477b27882aa45c51475352f56a
adambuttrick <adambuttrick@gmail.com>
2024-04-19 13:37:20 -0700

    Revert "Update add_languages_to_data_dump.py"
    This reverts commit 3b590af7ca9adea8e71238654fd8344b7e539146.

--- update_data_dump/add_languages_to_data_dump/add_languages_to_data_dump/add_languages_to_data_dump.py
+++ update_data_dump/add_languages_to_data_dump/add_languages_to_data_dump/add_languages_to_data_dump.py
@@ -3,7 +3,6 @@ import re
 import csv
 import json
 import argparse
-from datetime import datetime
 
 
 def parse_csv(file_path):
@@ -35,11 +34,7 @@ def save_individual_json(directory, record):
         json.dump(record, file, indent=4, ensure_ascii=False)
 
 
-def update_last_mod(record, date):
-    record['admin']['last_modified']['date'] = date
-
-
-def update_json_records(data_dump, csv_data, updates_dir, date):
+def update_json_records(data_dump, csv_data, updates_dir):
     for record in data_dump:
         record_id = record['id']
         if record_id in csv_data:
@@ -47,7 +42,6 @@ def update_json_records(data_dump, csv_data, updates_dir, date):
                 for name, lang in csv_data[record_id]:
                     if name_obj['value'] == name:
                         name_obj['lang'] = lang
-            update_last_mod(record, date)
             save_individual_json(updates_dir, record)
     return data_dump
 
@@ -67,31 +61,20 @@ def parse_args():
     parser.add_argument('-o', '--output_file', required=True,
                         help='Path to the output JSON file')
     parser.add_argument('-u', '--updates_dir', default='updates',
-                        help='Directory to save individual updated JSON files')
-    parser.add_argument('-t', '--date', required=True,
-                        help='Date to update the last_modified field (YYYY-MM-DD)')
+                        help='Directory to save individual JSON files')
     return parser.parse_args()
-
-
-def validate_date(date_string):
-    try:
-        datetime.strptime(date_string, '%Y-%m-%d')
-    except ValueError:
-        raise argparse.ArgumentTypeError(
-            f"Invalid date format: {date_string}. Expected format: YYYY-MM-DD")
 
 
 def main():
     args = parse_args()
-    validate_date(args.date)
     csv_data = parse_csv(args.input_file)
     data_dump = parse_json(args.data_dump_file)
     os.makedirs(args.updates_dir, exist_ok=True)
     updated_data_dump = update_json_records(
-        data_dump, csv_data, args.updates_dir, args.date)
+        data_dump, csv_data, args.updates_dir)
     save_json(args.output_file, updated_data_dump)
-    print(f"Updated JSON records saved to {args.output_file}")
-    print(f"Individual updated JSON files saved in {args.updates_dir}")
+    print(f'Updated JSON records saved to {args.output_file}')
+    print(f'Individual JSON files saved in {args.updates_dir} directory')
 
 
 if __name__ == '__main__':
