900051ad1bbfc0d33ecf6bbb13959314537257a0
adambuttrick <adambuttrick@gmail.com>
2024-05-02 15:43:25 -0700

    Update move_to_done.py
    Fix column name

--- after_release/move_to_done/move_to_done.py
+++ after_release/move_to_done/move_to_done.py
@@ -9,10 +9,10 @@ def move_to_done():
     repo = g.get_repo("ror-community/ror-updates")
     project = repo.get_projects()[0]
     columns = project.get_columns()
-    approved_column = [column for column in columns if column.name ==
-                       'Ready for production release'][0]
+    approved_column = [
+        column for column in columns if column.name == 'Ready for production release'][0]
     done_column = [column for column in columns if column.name ==
-                   'Done (released on production)'][0]
+                   'Done (Released on Production)'][0]
     approved_cards = approved_column.get_cards()
     for card in approved_cards:
         card.move('bottom', done_column)
