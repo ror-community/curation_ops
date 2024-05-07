2eea7fc044e3d1b18e9204359d568daa9fe68ad0
adambuttrick <adambuttrick@gmail.com>
2024-05-07 12:15:11 -0700

    Update create_issues.py
    Fix issue body formatting

--- create_issues_cl/create_issues/create_issues.py
+++ create_issues_cl/create_issues/create_issues.py
@@ -54,29 +54,30 @@ def create_new_record_issue():
 
     issue_title = f"Add a new organization to ROR: {name}"
     issue_body = f"""
-    Summary of request: Add a new organization to ROR
-    Name of organization: {name}
-    Website: {website}
-    Domains: {website}
-    Link to publications: {link_to_publications}
-    Organization type: {organization_type}
-    Wikipedia page: {wikipedia_page}
-    Wikidata ID: {wikidata_id}
-    ISNI ID: {isni_id}
-    GRID ID: {grid_id}
-    Crossref Funder ID: {crossref_funder_id}
-    Other names for the organization: {other_names_for_the_organization}
-    Aliases: {aliases}
-    Labels: {labels}
-    Acronym/abbreviation: {acronym_abbreviation}
-    Related organizations: {related_organizations}
-    City: {city}
-    Country: {country}
-    Geonames ID: {geonames_id}
-    Year established: {year_established}
-    How will a ROR ID for this organization be used? {how_will_a_ror_id_for_this_organization_be_used}
-    Other information about this request: {other_information_about_this_request}
-    """
+Summary of request: Add a new organization to ROR
+
+Name of organization: {name}
+Website: {website}
+Domains: {website}
+Link to publications: {link_to_publications}
+Organization type: {organization_type}
+Wikipedia page: {wikipedia_page}
+Wikidata ID: {wikidata_id}
+ISNI ID: {isni_id}
+GRID ID: {grid_id}
+Crossref Funder ID: {crossref_funder_id}
+Other names for the organization: {other_names_for_the_organization}
+Aliases: {aliases}
+Labels: {labels}
+Acronym/abbreviation: {acronym_abbreviation}
+Related organizations: {related_organizations}
+City: {city}
+Country: {country}
+Geonames ID: {geonames_id}
+Year established: {year_established}
+How will a ROR ID for this organization be used? {how_will_a_ror_id_for_this_organization_be_used}
+Other information about this request: {other_information_about_this_request}
+"""
     issue_labels = ["triage needed", "level 1", "new record", "jaguar"]
     return create_github_issue(issue_title, issue_body, issue_labels)
 
@@ -90,12 +91,13 @@ def create_update_record_issue():
 
     issue_title = f"Modify the information in an existing ROR record: {name} - {ror_id}"
     issue_body = f"""
-    Summary of request: Modify the information in an existing ROR record
-    Name of organization: {name}
-    ROR ID: {ror_id}
-    Which part of the record needs to be changed? {fields}
-    Description of change: {description}
-    """
+Summary of request: Modify the information in an existing ROR record
+
+Name of organization: {name}
+ROR ID: {ror_id}
+Which part of the record needs to be changed? {fields}
+Description of change: {description}
+"""
     issue_labels = ["triage needed", "level 2", "update record", "jaguar"]
     return create_github_issue(issue_title, issue_body, issue_labels)
 
